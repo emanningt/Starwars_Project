6d09d2a9199f60d095924c75bd95c57965b1fdcf
Eric Manning <emanningmusic@yahoo.com>
2019-12-05 12:27:09 -0800

    adding an accessor, initialize, and all method in movie file

--- lib/StarWars_Project/movie.rb
+++ lib/StarWars_Project/movie.rb
@@ -1,5 +1,21 @@
 class StarWarsProject::Movie
 
+attr_accessor :title
 
+@@all = []
+
+def initialize(title)
+  @title = title
+  self.save
+  #add other atrubutes
+end
+
+def self.all
+  @@all
+end
+
+def save
+  @@all << self
+end
 
 end
