b3aff6f5a8fbc6ec77718fe61e372ff05d9f8308
Eric Manning <emanningmusic@yahoo.com>
2019-12-06 09:06:56 -0800

    remove inroducton from bin a adding to intro. trying to add list of char

--- lib/StarWars_Project/cli.rb
+++ lib/StarWars_Project/cli.rb
@@ -3,9 +3,28 @@
 class StarWarsProject::CLI
 
   def intro
+    puts "Hello! What is your name?"
+    name = gets.chomp()
+    puts ("Hello " + name + ", are you ready to learn more about starwars?" )
+    puts "Well you're in the right place thats for sure."
+    StarWarsProject::API.character_list
+    #binding.pry
+    menu
   ending
   end
 
+  def menu
+    input = nil
+
+    puts "Characters:"
+    list_of_characters
+  end
+
+  def list_of_characters
+    StarWarsProject::characters.all.each do |char, index|
+      puts "#{char} #{index}"
+    end
+  end
 
   def ending
     puts "May the force be with you!"
