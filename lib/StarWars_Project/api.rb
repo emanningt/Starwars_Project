982ddcf82b459e89909f6cfff313965da675ced8
Eric Manning <emanningmusic@yahoo.com>
2019-12-06 11:14:03 -0800

    udated to api class

--- lib/StarWars_Project/api.rb
+++ lib/StarWars_Project/api.rb
@@ -8,9 +8,22 @@ class StarWarsProject::API
     characters = RestClient.get('https://swapi.co/api/people/')
     @character_hash = JSON.parse(characters)
     @character_hash["results"].each do |chars|
-          StarWarsProject::Characters.new_from_json(chars)
+      #self is the class
+        self.new_from_json(chars)
           #binding.pry
     end
+  end
+
+  def self.new_from_json(char_info)
+    StarWarsProject::Characters.new(
+      char_info["name"],
+      char_info["homeworld"],
+      char_info["gender"],
+      char_info["birth_year"],
+      char_info["hair_color"],
+      char_info["skin_color"],
+      char_info["height"]
+    )
   end
 end
 
