4dcf1b44f7366bd73e02c9fedb64a60f18908187
Eric Manning <emanningmusic@yahoo.com>
2019-12-06 08:38:26 -0800

    Building my initialized method

--- lib/StarWars_Project/characters.rb
+++ lib/StarWars_Project/characters.rb
@@ -1,14 +1,25 @@
 class StarWarsProject::Characters
 
-attr_accessor :character_hash, :characters
+  attr_accessor :character_hash, :characters
 
-@@all = []
+  @@all = []
 
-def self.new_from_json(char_info)
-  self.new(
-    char_info["name"],
-    char_info["homeworld"],
-    char_info["gender"],
-    char_info["birth_year"]
-  )
-end
+  def self.new_from_json(char_info)
+    self.new(
+      char_info["name"],
+      char_info["homeworld"],
+      char_info["gender"],
+      char_info["birth_year"]
+    )
+  end
+
+  def initialize(character_name, homeworld, gender, birth_year)
+    @character_name = character_name
+    @homeworld = homeworld
+    @gender = gender
+    @birth_year = birth_year
+    @@all << self
+  end
+
+
+end 
