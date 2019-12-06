1e8e5646a3dfceec025b9ea8045677b5d6ecfedc
Eric Manning <emanningmusic@yahoo.com>
2019-12-06 08:20:35 -0800

    changing form movie class to character class.

--- lib/StarWars_Project/cli.rb
+++ lib/StarWars_Project/cli.rb
@@ -2,19 +2,9 @@
 
 class StarWarsProject::CLI
 
-  def intro
-    StarWarsProject::API.get_films
-    movies = StarWarsProject::Movie.all
-    binding.pry
 
-    menu
-    ending
-  end
 
-  def list
-    StarWarsProject::Movie.all.each.with_index(1) do |movie, i|
-     puts "#{i}. #{movie.title}."
-   end
+
   end
 
   def details
@@ -22,11 +12,8 @@ class StarWarsProject::CLI
   end
 # add a lits method for move.all
 #add detial method to show the details
-  
 
-      end
-    end
-  end
+
 
   def ending
     puts "May the force be with you!"
