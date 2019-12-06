51cff29731fac337dc653843c45a6d53ffc1707a
Eric Manning <emanningmusic@yahoo.com>
2019-12-06 10:05:44 -0800

    trying an if else rathern the while loop. still need char details

--- lib/StarWars_Project/cli.rb
+++ lib/StarWars_Project/cli.rb
@@ -14,7 +14,28 @@ class StarWarsProject::CLI
     input = nil
     puts "Characters List:"
     list_of_characters
+    input = gets.strip.downcase
+    if input == "exit"
+      ending
+    else
+      num = input.to_i
+      if nim != 0
+        char_input = StarWarsProject::Character.find(num)
+        character_details(char_input)
+        puts "would you like to see informatoin fror another character? yes/no"
+        anser = gets.strip.downcase
+        if anser == "yes" || anser == "y"
+          menu
+        else
+        ending
+      end
+      puts " I dont understand that selecction"
+      puts " please select a character bumber, or typed exit to leave the program."
+      menu
+      #binding.pry
+    end
   end
+
 
   def list_of_characters
     StarWarsProject::Characters.all.each.with_index(1) do |char, index|
