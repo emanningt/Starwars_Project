11f2b6544b05ae74123e1ced881388043778ab23
Eric Manning <emanningmusic@yahoo.com>
2019-12-06 08:34:40 -0800

    Rebuilding my character class

--- lib/StarWars_Project/characters.rb
+++ lib/StarWars_Project/characters.rb
@@ -1,29 +1,14 @@
-class StarWarsProject::Movie
+class StarWarsProject::Characters
 
-attr_accessor :title, :opening_crawl, :director, :release_date
+attr_accessor :character_hash, :characters
 
 @@all = []
 
-  def self.new_info(movie_info)
-    self.new(
-      movie_info["title"]
-    )
-
-def initialize(title, opening_crawl, director, release_date)
-  @title = title
-  @opening_crawl = opening_crawl
-  @director = director
-  @release_date = release_date
-  self.save
-  #add other atrubutes
-end
-
-def self.all
-  @@all
-end
-
-def save
-  @@all << self
-end
-
+def self.new_from_json(char_info)
+  self.new(
+    char_info["name"],
+    char_info["homeworld"],
+    char_info["gender"],
+    char_info["birth_year"]
+  )
 end
